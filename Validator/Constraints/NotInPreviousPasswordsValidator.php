7a7ebf7584040bcc10ea227e9cde5ec43548438b
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2015-06-10 12:13:09 +0200

    Update NotInPreviousPasswordsValidator.php

--- Validator/Constraints/NotInPreviousPasswordsValidator.php
+++ Validator/Constraints/NotInPreviousPasswordsValidator.php
@@ -11,21 +11,31 @@ use Symfony\Component\Routing\Router;
 class NotInPreviousPasswordsValidator extends ConstraintValidator
 {
 
-    public function __construct( EntityManager $em, SecurityContext $security, $encoder_service)
-    {
+    public function __construct( EntityManager $em, SecurityContext $security, $encoder_service, RequestStack $requestStack, UserManager $fosUserManager)
+    {   
         $this->em = $em;
         $this->security = $security;
         $this->encoder_service = $encoder_service;
-    }
+        $this->requestStack = $requestStack;
+        $this->fosUserManager = $fosUserManager;
+
+    }   
 
 
     public function validate($value, Constraint $constraint)
-    {
-        $user = $this->security->getToken()->getUser();
-        
+    {   
+        $request = $this->requestStack->getCurrentRequest();
+        if (null != $request->get("token", null)) {
+            $user = $this->fosUserManager->findUserByConfirmationToken($request->get("token"));
+        }   
+        else {
+            $user = $this->security->getToken()->getUser();
+        }   
+
         if (!$user) {
             return;
-        }
+        }   
+
         try {
             $encoder = $this->encoder_service->getEncoder($user);
             $encodedValue = $encoder->encodePassword($value, $user->getSalt());
@@ -33,13 +43,13 @@ class NotInPreviousPasswordsValidator extends ConstraintValidator
             $passwordMatch= $this->em->getRepository("ACSEOChangePasswordBundle:PasswordHistory")
                 ->findBy(array("user" =>$user, "password" => $encodedValue )); 
 
-            if(sizeof($passwordMatch) != 0) {  
+            if(sizeof($passwordMatch) != 0) {   
                 $this->context->addViolation($constraint->message);
-            }
-        }
+            }   
+        }   
         catch (\Exception $e) {
             // Known case : No encoder has been configured for account "anon.". for user Reset action
             return;
-        }
-    }
+        }   
+    }   
 }
